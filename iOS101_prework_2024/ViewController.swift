cbf679b36a06630b18fccaaa4b6d5544b6185c90
dafneMena <dafneg2306@icloud.com>
2024-11-22 17:15:47 -0800

    code portion done

--- iOS101_prework_2024/ViewController.swift
+++ iOS101_prework_2024/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        holidayImg.isHidden = true
     }
 
-
+    @IBAction func resetBtn(_ sender: UIButton) {
+        holidayImg.isHidden = true
+        self.view.backgroundColor = UIColor.white
+    }
+    @IBOutlet weak var holidayImg: UIImageView!
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        
+        holidayImg.isHidden = false
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
 }
 
